0dc93e4b3aca3ceec0de70248c032c6b3c70a753
commandblock2 <tony_hu@live.com>
2026-01-31 18:14:06 +0800

    chore: extra-clean

--- src/routes/clickgui/ClickGui.svelte
+++ src/routes/clickgui/ClickGui.svelte
@@ -1,12 +1,8 @@
 <script lang="ts">
-    import type {GroupedModules, Module} from "../../integration/types";
-    import Panel from "./Panel.svelte";
-    import Search from "./Search.svelte";
-    import Description from "./Description.svelte";
-    import {fade} from "svelte/transition";
-    import {onMount} from "svelte";
-    import {getModules} from "../../integration/rest";
-    import {groupByCategory} from "../../integration/util";
+    import type { GroupedModules, Module } from "../../integration/types";
+    import { onMount } from "svelte";
+    import { getModules } from "../../integration/rest";
+    import { groupByCategory } from "../../integration/util";
 
     let categories = $state<GroupedModules>({});
     let modules = $state<Module[]>([]);
@@ -17,11 +13,5 @@
     });
 </script>
 
-<div class="clickgui" transition:fade|global={{ duration: 200 }}>
-    <Description/>
-    <Search modules={structuredClone($state.snapshot(modules))}/>
-
-    {#each Object.entries(categories) as [category, modules], panelIndex (category)}
-        <Panel {category} {modules} {panelIndex}/>
-    {/each}
+<div class="clickgui">
 </div>
